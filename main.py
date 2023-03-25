75c20f4f6b5a219e96b0ca55e834272b89f2090e
maresyp <mattq.2000@icloud.com>
2023-03-25 17:59:24 +0100

    Add mock user generation

---
+++ main.py
@@ -0,0 +1,4 @@
+""" Main module for the project. """
+
+if __name__ == "__main__":
+    pass
---
+++ src/utils/mock_user/generator.py
@@ -0,0 +1,66 @@
+from abc import ABC, abstractmethod
+from faker import Faker
+from faker.providers import BaseProvider
+from typing import Iterable
+import random
+
+class Generator(ABC):
+    """ Abstract class for generating mock users. """
+
+    @abstractmethod
+    def generate(self, *args, **kwargs):
+        """ Abstract method for generating mock users. """
+        pass
+
+class UserGenerator(Generator, BaseProvider):
+    """ Class for generating mock users. """
+
+    def __init__(self, tags: list = None):
+        """ Constructor for UserGenerator class. """
+        self.__faker = Faker()
+        Faker.seed(2137)
+        random.seed(2137)
+
+        self.__tags = tags
+        if self.__tags is None:
+            self.__tags = ["Python", "C", "C++", "Java"]
+
+    def generate(self, amount: int = 1) -> Iterable[dict]:
+        """ Method for generating mock users. """
+        if amount < 1:
+            raise ValueError("Amount must be greater than 0.")
+
+        for _ in range(amount):
+            data = dict()
+            data["gender"] = random.choice(["M", "F"])
+            data["name"] = self.generate_name(data["gender"])
+            data["email"] = self.generate_email(data["name"])
+            data["pwd"] = self.generate_password()
+            data["age"] = random.randint(18, 69)
+            data["tags"] = random.sample(self.__tags, random.randint(1, len(self.__tags)))
+            data["bio"] = self.generate_bio()
+
+            yield data
+
+    def generate_name(self, gender: str) -> str:
+        """ Method for generating a random name. """
+        if gender == "M":
+            return f'{self.__faker.first_name_male()} {self.__faker.last_name_male()}'
+        return f'{self.__faker.first_name_female()} {self.__faker.last_name_female()}'
+
+    def generate_email(self, name: str) -> str:
+        """ Method for generating a random email from given name. """
+        return f'{name.replace(" ", ".")}{random.randint(0, 2137)}@{self.__faker.free_email_domain()}'
+
+    def generate_password(self) -> str:
+        """ Method for generating a random password. """
+        return self.__faker.password(length=25, special_chars=True, digits=True, upper_case=True, lower_case=True)
+
+    def generate_bio(self) -> str:
+        """ Method for generating a random bio. """
+        return self.__faker.text(max_nb_chars=150)
+
+if __name__ == "__main__":
+    user_generator = UserGenerator()
+    for user in user_generator.generate(amount=50):
+        print(user)
