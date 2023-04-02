e673365d6f834cbdd9bf49df9aa5f1597d0d4138
gruszm <mariuszgruszczynski11@gmail.com>
2023-04-02 10:13:24 +0200

    Crawler v2 - listing of pastes IDs to download

--- src/utils/crawler.py
+++ src/utils/crawler.py
@@ -1,20 +1,36 @@
 import requests
 import random
 import string
+from bs4 import BeautifulSoup
 
-while True:
+def get_list_of_paste_ids(wanted_lang: str):
+    url = "https://pastebin.com/archive/%s" % wanted_lang
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    pastes_list = []
+
+    for paste in soup.select("table.maintable tr td:nth-of-type(1) a[href]"):
+        paste_id = paste["href"].split("/")[-1]
+        pastes_list.append(paste_id)
+
+    return pastes_list
+
+def get_random_code(wanted_lang: str):
+    paste_ids = get_list_of_paste_ids(wanted_lang)
+
+    random_url = random.choice(paste_ids)
+
     # Replace the URL with the raw Pastebin URL of the code you want to download
-    url = "https://pastebin.com/raw/%s" % (''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=8)))
-    # url = 'https://pastebin.com/raw/Pj04irL5' # Test URL, which should exist and be properly downloaded
+    url = 'https://pastebin.com/raw/%s' % random_url
     print(url)
 
     # Send a GET request to the URL to get the code
     response = requests.get(url)
 
     # Save the code to a file
-    with open("code.py", "w") as f:
-        f.write(response.text)
+    with open("%s.py" % random_url, "wb") as f:
+        f.write(response.content)
 
     print(response.status_code)
-    if response.status_code != 404:
-        break
+
+get_random_code('python')
