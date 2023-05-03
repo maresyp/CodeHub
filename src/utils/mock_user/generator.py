a498e99a37525bf55fc10106c58eb94acb0f2792
maresyp <mattq.2000@icloud.com>
2023-05-03 20:13:09 +0200

    make code_snippets dictionary for clarity

--- src/utils/mock_user/generator.py
+++ src/utils/mock_user/generator.py
@@ -39,7 +39,7 @@ class UserGenerator(Generator, BaseProvider):
             data["pwd"] = self.generate_password()
             data["age"] = random.randint(18, 69)
             data["tags"] = random.sample(self.__tags, random.randint(1, len(self.__tags)))
-            data["code_snippets"] = [self.generate_code_snippet(tag) for tag in data["tags"]]
+            data["code_snippets"] = {tag: [self.generate_code_snippet(tag) for tag in data["tags"]] for tag in data["tags"]}
             data["bio"] = self.generate_bio()
 
             yield data
