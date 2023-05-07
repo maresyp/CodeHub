1ed9ca0bf5d5d2749bca49344a119613d31a7f46
maresyp <mattq.2000@icloud.com>
2023-05-07 15:57:07 +0200

    code refactoring

--- utils/anti_plagiarism/PlagiarismChecker.py
+++ utils/anti_plagiarism/PlagiarismChecker.py
@@ -3,18 +3,38 @@ from enum import Enum
 from collections import Counter
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import math
+import numpy as np
 
 
 class PlagiarismMethod(Enum):
     """ Enum for plagiarism checking methods """
     BOW = 1  # Bag of words
+    TFIDF = 2  # TF-IDF
 
 
 @dataclass(frozen=True)
 class PlagiarismResult:
     """ Class for storing plagiarism check result. """
     method: PlagiarismMethod
-    percentage: float
+    jaccard_similarity: float
+    euclidian_distance: float
+    cosine_similarity: float
+
+    def judge_result(
+        self,
+        jaccard_threshold: float = 0.8,
+        euclidian_threshold: float = 0.8,
+        cosine_threshold: float = 0.8
+    ) -> bool:
+        """ Judge if the result is plagiarized """
+        if self.jaccard_similarity > jaccard_threshold:
+            return True
+        if self.euclidian_distance > euclidian_threshold:
+            return True
+        if self.cosine_similarity > cosine_threshold:
+            return True
+
+        return False
 
 
 class PlagiarismChecker:
@@ -26,19 +46,28 @@ class PlagiarismChecker:
     def check_code(self, code: str, method: set[PlagiarismMethod]) -> set[PlagiarismResult]:
         """ Checks plagiarism in the code using all methods specified """
         results: set[PlagiarismResult] = set()
+        embeddings = []
 
         if PlagiarismMethod.BOW in method:
-            bow = self.__get_bow(code)
-            for b in bow:
-                print(b)
+            bow = self.__create_bow_embeddings(code)
+            embeddings.append((PlagiarismMethod.BOW, bow))
+        if PlagiarismMethod.TFIDF in method:
+            tfidf = self.__create_tfid_embeddings(code)
+            embeddings.append((PlagiarismMethod.TFIDF, tfidf))
 
-        print(f"jaccard: {self.__jaccard_similarity(set(self.reference_code.split()), set(code.split()))}")
+        # calculate similarity for each method
+        jaccard = self.__jaccard_similarity(set(self.reference_code.split()), set(code.split()))
+        for embedding in embeddings:
+            method, emb = embedding
 
-        embeddings = self.__create_embeddings(code)
+            euclidian = self.__normalized_euclidian_distance(emb[0], emb[1])
+            cosine = self.__cosine_similarity(emb[0], emb[1])
+
+            results.add(PlagiarismResult(method, jaccard, euclidian, cosine))
 
         return results
 
-    def __get_bow(self, code: str) -> tuple[list[int], list[int]]:
+    def __construct_bow(self, code: str) -> tuple[list[int], list[int]]:
         """ Returns bag of words for reference code and tested code """
         reference_words = Counter(self.reference_code.split())
         code_words = Counter(code.split())
@@ -60,33 +89,26 @@ class PlagiarismChecker:
     @staticmethod
     def __normalized_euclidian_distance(x, y) -> float:
         """ Calculates Euclidian distance """
-        dist = math.sqrt(sum(pow(a-b, 2) for a, b in zip(x, y)))
+        dist = math.sqrt(sum(pow(a - b, 2) for a, b in zip(x, y)))
         # normalization to 0-1 range
         return 1 / math.exp(dist)
 
     @staticmethod
     def __cosine_similarity(x, y) -> float:
         """ Calculates cosine similarity """
-        dot_product = sum(a*b for a, b in zip(x, y))
+        dot_product = sum(a * b for a, b in zip(x, y))
         magnitude = math.sqrt(sum(pow(a, 2) for a in x)) * math.sqrt(sum(pow(b, 2) for b in y))
         return dot_product / magnitude
 
-    def __create_embeddings(self, code: str) -> tuple[list[int], list[int]]:
+    def __create_bow_embeddings(self, code: str) -> tuple[np.ndarray, np.ndarray]:
         """ Creates embeddings for reference code and tested code """
-        vect = CountVectorizer(lowercase=False)
-        # create bag of words for reference code and tested code
-        embeddings = vect.fit_transform([self.reference_code, code])
-        print(vect.get_feature_names_out())
-        print(embeddings.toarray())
-        print(f"euclidian: {self.__normalized_euclidian_distance(embeddings.toarray()[0], embeddings.toarray()[1])}")
-        print(f"cosine: {self.__cosine_similarity(embeddings.toarray()[0], embeddings.toarray()[1])}")
+        bow = CountVectorizer(lowercase=False).fit_transform([self.reference_code, code]).toarray()
+        return bow[0], bow[1]
 
-        tfidf = TfidfVectorizer(lowercase=False).fit_transform([self.reference_code, code])
-        print(f"euclid tfidf: {self.__normalized_euclidian_distance(tfidf.toarray()[0], tfidf.toarray()[1])}")
-        print(f"cosine tfidf: {self.__cosine_similarity(tfidf.toarray()[0], tfidf.toarray()[1])}")
-
-        samples, features = tfidf
-        return samples, features
+    def __create_tfid_embeddings(self, code: str) -> tuple[np.ndarray, np.ndarray]:
+        """ Creates embeddings for reference code and tested code """
+        embeddings = TfidfVectorizer(lowercase=False).fit_transform([self.reference_code, code]).toarray()
+        return embeddings[0], embeddings[1]
 
 
 if __name__ == '__main__':
@@ -114,4 +136,6 @@ if __name__ == '__main__':
     """
 
     checker = PlagiarismChecker(doc1)
-    print(checker.check_code(doc2, {PlagiarismMethod.BOW}))
+    result = checker.check_code(doc2, {PlagiarismMethod.TFIDF, PlagiarismMethod.BOW})
+    for r in result:
+        print(f"{r.judge_result()} {r}")
