b24e1dd66dca3cba5772fe17c795475e1c3060f9
maresyp <mattq.2000@icloud.com>
2023-05-12 15:17:24 +0200

    add Profile model for User

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import uuid
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(max_length=200, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    profile_img = models.ImageField(null=True, blank=True, upload_to='images/profiles/', default='images/profiles/user-default.png')
+    social_github = models.CharField(max_length=200, null=True, blank=True)
+    social_twitter = models.CharField(max_length=200, null=True, blank=True)
+    social_linkedin = models.CharField(max_length=200, null=True, blank=True)
+    social_youtube = models.CharField(max_length=200, null=True, blank=True)
+    social_website = models.CharField(max_length=200, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    def __str__(self):
+        return str(self.user.username)
