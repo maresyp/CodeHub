7d4f74ce502d08c1bf1d42dbd5ad1b333d7df393
maresyp <mattq.2000@icloud.com>
2023-05-12 21:10:33 +0200

    fix admin site

--- CodeHub/settings.py
+++ CodeHub/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
--- main.py
+++
@@ -1,4 +0,0 @@
-""" Main module for the project. """
-
-if __name__ == "__main__":
-    pass
