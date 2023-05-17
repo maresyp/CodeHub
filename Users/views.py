fffdc1740c98b90bfd4d069d35904b1d995ed4d8
maresyp <mattq.2000@icloud.com>
2023-05-17 20:49:16 +0200

    add flash messages for user

--- Users/views.py
+++ Users/views.py
@@ -3,7 +3,7 @@ from .models import Profile
 from .models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-
+from django.contrib import messages
 # Create your views here.
 
 def login_user(request):
@@ -14,20 +14,21 @@ def login_user(request):
         try:
             user = User.objects.get(username=username)
         except Exception as e:
-            print('User does not exist')
+            messages.error(request, 'Username does not exist')
 
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('profiles')
         else:
-            print('Username OR password is incorrect')
+            messages.error(request, 'Username OR password is incorrect')
 
     return render(request, 'Users/login_register.html')
 
 
 def logout_user(request):
     logout(request)
+    messages.success(request, 'User was logged out')
     return redirect('login')
 
 
