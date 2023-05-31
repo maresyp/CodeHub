2e92dc4aae631b443eb1a79769d1e1d3abe4bf05
maresyp <mattq.2000@icloud.com>
2023-05-31 12:16:35 +0200

    implement finding friends for chat

--- Chat/views.py
+++ Chat/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from Users.models import User
+from Users.models import User, Matches
+from .models import Message
+from django.db.models import Q
 # Create your views here.
 
 @login_required(login_url='login')
 def lobby(request):
-    # TODO: find only users that are matching and sort them based on messages
-    context = {'users': User.objects.exclude(id=request.user.id)}
+    matches = Matches.objects.filter(
+        Q(first_user=request.user, first_status=True) |
+        Q(second_user=request.user, second_status=True)
+    )
+
+    friends = User.objects.filter(
+        Q(id__in=matches.values_list('first_user', flat=True))
+        | Q(id__in=matches.values_list('second_user', flat=True))
+    ).exclude(id=request.user.id)
+
+    # TODO: find friend with most recent message and get last 10 messages
+    # TODO: find top 10 recent friends ( based on message timestamp ) and get most recent message for each friend
+
+    context = {'friends': friends}
     return render(request, 'Chat/lobby.html', context=context)
