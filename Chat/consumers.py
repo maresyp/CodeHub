9ec63a0b5fce9223cb9f749ca5e99479b0d761fa
maresyp <mattq.2000@icloud.com>
2023-06-04 17:33:24 +0200

    implement chatting with different users

--- Chat/consumers.py
+++ Chat/consumers.py
@@ -1,8 +1,10 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
+from django.db.models import Q
 from django.contrib.auth.models import User
 from .models import Message
 import json
+from asgiref.sync import sync_to_async
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -19,10 +21,29 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-    async def receive(self, text_data):
+    async def receive(self, text_data=None, bytes_data=None):
+        if text_data is None:
+            return
+
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        recipient = await self.get_user(text_data_json['recipient'])
+        try:
+            match text_data_json['type']:
+                case 'chat-message':
+                    await self.chat_message_handler(text_data_json)
+                case 'recipient-change':
+                    await self.recipient_change_handler(text_data_json)
+        except KeyError:
+            return
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def chat_message_handler(self, data):
+        message = data['message']
+        recipient = await self.get_user(data['recipient'])
         user = await self.get_user(self.scope['user'].id)
 
         # TODO: check if user and recipiend are matched before saving the message
@@ -39,40 +60,46 @@ class ChatConsumer(AsyncWebsocketConsumer):
             }
         )
 
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',  # chat_message method will be called
-                'message': message,
-                'sender': user,
-                'recipient': recipient
-            }
-        )
+    async def recipient_change_handler(self, data):
+        await self.send_messages(data['recipient'])
 
     async def chat_message(self, event):
-        message = event['message']
-        sender = event['sender']
-        recipient = event['recipient']
-
         await self.send(text_data=json.dumps({
-            'type': 'chat',
-            'message': message,
-            'sender': sender.id,
-            'recipient': recipient.id,
+            'type': 'chat-single-message',
+            'message': event['message'],
+            'sender': event['sender'].id,
+            'recipient': event['recipient'].id,
             'timestamp': 'now'
         }))
 
-    async def send_messages(self, messages):
-        for message in messages:
-            data = await self.get_message_data(message)
-            timestamp_str = data['timestamp'].strftime('%Y-%m-%d %H:%M:%S')
-            await self.send(text_data=json.dumps({
-                'type': 'chat',
-                'message': data['message'],
-                'sender': data['sender'],
-                'recipient': data['recipient'],
-                'timestamp': timestamp_str
-            }))
+    async def send_messages(self, recipient_id):
+        print(f"Sending messages to {recipient_id}")
+
+        @sync_to_async
+        def get_messages():
+            start_index = (self.page_number - 1) * self.PAGE_SIZE
+            end_index = start_index + self.PAGE_SIZE
+
+            messages = Message.objects.filter(
+                Q(sender_id=self.scope['user'], recipient_id=recipient_id) |
+                Q(sender_id=recipient_id, recipient_id=self.scope['user'])
+            ).order_by('-send_timestamp')[start_index:end_index]
+
+            messages_data = {
+                str(msg.message_id): {
+                    'message': msg.body,
+                    'sender': msg.sender_id.id,
+                    'recipient': msg.recipient_id.id,
+                    'timestamp': msg.send_timestamp.strftime('%Y-%m-%d %H:%M:%S')
+                } for msg in messages
+            }
+
+            return messages_data
+
+        await self.send(text_data=json.dumps({
+            'type': 'chat-new-window',
+            'messages': await get_messages(),
+        }))
 
     @database_sync_to_async
     def get_user(self, id: str):
@@ -80,25 +107,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     @database_sync_to_async
     def save_message(self, sender, recipient, message):
-        print(recipient)
         Message.objects.create(
             sender_id=sender,
             recipient_id=User.objects.get(id=recipient),
             body=message
         )
 
-    @database_sync_to_async
-    def get_messages(self):
-        start_index = (self.page_number - 1) * self.PAGE_SIZE
-        end_index = start_index + self.PAGE_SIZE
-        # TODO: filter messages by sender and recipient
-        return list(Message.objects.order_by('send_timestamp')[start_index:end_index])
-
-    @database_sync_to_async
-    def get_message_data(self, message):
-        return {
-            'message': message.body,
-            'sender': message.sender_id.username,
-            'recipient': message.recipient_id.username,
-            'timestamp': message.send_timestamp
-        }
--- static/js/chat.js
+++ static/js/chat.js
@@ -5,14 +5,22 @@ let friends = document.querySelectorAll('a[user-uuid]');
 
 const chatSocket = new WebSocket(url)
 
+function appendMessage(message) {
+    let messages = document.getElementById('messages')
+    console.log('Appending message:', message)
+    messages.insertAdjacentHTML(
+        'beforeend',
+        message
+    )
+}
+
 // TODO: handle case when user has no friends yet
 
 chatSocket.onmessage = function (e) {
     let data = JSON.parse(e.data)
     console.log('Data:', data) // TODO: remove this
 
-    if (data.type === 'chat') {
-        let messages = document.getElementById('messages')
+    if (data.type === 'chat-single-message') {
         let message_body = null
 
         if (data.sender == recipient) {
@@ -20,10 +28,22 @@ chatSocket.onmessage = function (e) {
         } else {
             message_body = `<p style="text-align: right;">${data.message}</p>`
         }
-        messages.insertAdjacentHTML(
-            'beforeend',
-            message_body
-        )
+
+        appendMessage(message_body)
+    } else if (data.type === 'chat-new-window') {
+        [data.messages].forEach(msg => {
+            Object.keys(msg).forEach(key => {
+                let message_body = null
+
+                if (msg[key].sender == recipient) {
+                    message_body = `<p style="text-align: left;">${msg[key].message}</p>`
+                } else {
+                    message_body = `<p style="text-align: right;">${msg[key].message}</p>`
+                }
+
+                appendMessage(message_body)
+            })
+        })
     }
 }
 
@@ -36,12 +56,23 @@ form.addEventListener('submit', (e) => {
         'recipient': recipient
     }))
     form.reset()
+    appendMessage(`<p style="text-align: right;">${message}</p>`)
 })
 
 friends.forEach((friend) => {
     friend.addEventListener('click', (e) => {
         e.preventDefault()
         recipient = friend.getAttribute('user-uuid')
-        console.log('Other user uuid:', recipient)
+        console.log('Selected user with uuid:', recipient)
+
+        // send information to server that recipient has been changed
+        chatSocket.send(JSON.stringify({
+            'type': 'recipient-change',
+            'recipient': recipient
+        }))
+
+        // clear messages
+        let messages = document.getElementById('messages')
+        messages.innerHTML = ''
     })
 })
