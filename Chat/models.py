c5123033ed67a07abad75882521b4f3b5de8ad3e
maresyp <mattq.2000@icloud.com>
2023-07-06 09:56:47 +0200

    docs for Chat

--- Chat/models.py
+++ Chat/models.py
@@ -6,11 +6,24 @@ from Users.models import User
 # Create your models here.
 
 class Message(models.Model):
+    """
+    This Django model represents a chat message between users.
+
+    :param models.Model: Inherits from Django's Model class.
+    :type models.Model: class
+
+    :ivar UUIDField message_id: Unique ID for the message, auto-generated.
+    :ivar ForeignKey sender_id: Reference to User who sent the message.
+    :ivar ForeignKey recipient_id: Reference to User who is the recipient of the message.
+    :ivar TextField body: Body of the message.
+    :ivar BooleanField is_read: Field to mark whether the message has been read.
+    :ivar DateTimeField send_timestamp: Timestamp when the message was sent.
+    :ivar DateTimeField view_timestamp: Timestamp when the message was viewed.
+    """
     message_id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     sender_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender_id')
     recipient_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='recipient_id')
     body = models.TextField(max_length=5000)
-    # attachment =  ???
     is_read = models.BooleanField(default=False)
     send_timestamp = models.DateTimeField(auto_now_add=True)
     view_timestamp = models.DateTimeField(null=True, blank=True)
